c49d905c0eb81cef67250beec6b847cc89e879c0
Alan Wang <alan@tacen.app>
2022-05-13 22:15:17 +0800

    change table to mint history

--- src/views/Stake/components/ExternalStakePools/ExternalStakePools.tsx
+++ src/views/Stake/components/ExternalStakePools/ExternalStakePools.tsx
@@ -64,33 +64,64 @@ export const ExternalStakePools = () => {
       {isSmallScreen ? (
         <AllPools isSmallScreen={isSmallScreen} />
       ) : (
-        <Paper headerText={t`Farm Pool`}>
+        <Paper headerText={t`Mint History`}>
           <Table style={{ tableLayout: "fixed" }}>
             <TableHead className={styles.stakePoolHeaderText}>
               <TableRow>
-                <TableCell style={{ width: "250px", padding: "8px 0" }}>
-                  <Trans>Asset</Trans>
+                <TableCell style={{ width: "180px", padding: "8px 0" }}>
+                  <Trans>Time</Trans>
                 </TableCell>
 
                 <TableCell style={{ width: connected ? "100px" : "150px", padding: "8px 0" }}>
-                  <Trans>TVL</Trans>
+                  <Trans>Input Value</Trans>
                 </TableCell>
 
                 <TableCell style={{ width: connected ? "100px" : "150px", padding: "8px 0" }}>
-                  <Trans>APY</Trans>
+                  <Trans>Output Value</Trans>
                 </TableCell>
 
-                {connected && <TableCell style={{ width: "100px", padding: "8px 0" }}>{t`Balance`}</TableCell>}
+                <TableCell style={{ width: connected ? "100px" : "150px", padding: "8px 0" }}>
+                  <Trans>Avg. Price</Trans>
+                </TableCell>
+
+                <TableCell style={{ width: connected ? "100px" : "150px", padding: "8px 0" }}>
+                  <Trans>Wallet Address</Trans>
+                </TableCell>
               </TableRow>
             </TableHead>
 
-            <AllPools isSmallScreen={isSmallScreen} />
+            <AllMints isSmallScreen={isSmallScreen} />
           </Table>
         </Paper>
       )}
     </Zoom>
   );
 };
+
+const data = [
+  {
+    time: "5/13/2022, 9:44:10 PM",
+    inputValue: "17.08 WATER",
+    outputValue: "1.39 HYDR",
+    avgPrice: "$12.28",
+    walletAddress: "DwNW...wwtV",
+  },
+  {
+    time: "5/13/2022, 9:40:30 PM",
+    inputValue: "30,000 USDC",
+    outputValue: "2,442.61 HYDR",
+    avgPrice: "$12.28",
+    walletAddress: "7cTW...QLZY",
+  },
+];
+
+const AllMints = (props: { isSmallScreen: boolean }) => (
+  <>
+    {data.map(row => (
+      <Row {...row} />
+    ))}
+  </>
+);
 
 const AllPools = (props: { isSmallScreen: boolean }) => (
   <>
@@ -120,6 +151,48 @@ const AllPools = (props: { isSmallScreen: boolean }) => (
     ))}
   </>
 );
+
+const Row: React.FC<{
+  time: string;
+  inputValue: string;
+  outputValue: string;
+  avgPrice: string;
+  walletAddress: string;
+}> = props => {
+  return (
+    <TableRow>
+      <TableCell style={{ padding: "8px 0" }}>
+        <Typography gutterBottom={false} style={{ lineHeight: 1.4 }}>
+          {props.time}
+        </Typography>
+      </TableCell>
+
+      <TableCell style={{ padding: "8px 0" }}>
+        <Typography gutterBottom={false} style={{ lineHeight: 1.4 }}>
+          {props.inputValue}
+        </Typography>
+      </TableCell>
+
+      <TableCell style={{ padding: "8px 0" }}>
+        <Typography gutterBottom={false} style={{ lineHeight: 1.4 }}>
+          {props.outputValue}
+        </Typography>
+      </TableCell>
+
+      <TableCell style={{ padding: "8px 0" }}>
+        <Typography gutterBottom={false} style={{ lineHeight: 1.4 }}>
+          {props.avgPrice}
+        </Typography>
+      </TableCell>
+
+      <TableCell style={{ padding: "8px 0" }}>
+        <Typography gutterBottom={false} style={{ lineHeight: 1.4 }}>
+          {props.walletAddress}
+        </Typography>
+      </TableCell>
+    </TableRow>
+  );
+};
 
 const StakePool: React.FC<{ pool: ExternalPool; tvl?: number; apy?: number }> = props => {
   const { connected } = useWeb3Context();
